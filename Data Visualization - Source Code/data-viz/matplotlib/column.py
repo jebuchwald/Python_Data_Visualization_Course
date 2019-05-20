903c93b66fa1f94bda0a9f6f1761e93202d930e0
Jonathan Buchwald <jebuchwald@gmail.com>
2019-05-20 12:35:56 -0400

    editied column with initial stuff.

--- Data Visualization - Source Code/data-viz/matplotlib/column.py
+++ Data Visualization - Source Code/data-viz/matplotlib/column.py
@@ -7,14 +7,4 @@ import pickle
 with open('fruit-sales.pickle', 'rb') as f:
     data = pickle.load(f)
 
-# splitting a list of tuples into two lists
-fruit, num_sold = zip(*data)
-
-bar_coords = range(len(fruit))
-
-plt.bar(bar_coords, num_sold)
-# replace the x coords with the fruit names
-plt.xticks(bar_coords, fruit)
-plt.ylabel('Number of fruit (millions)')
-plt.title('Number of fruit sold (2017)')
-plt.show()
+print(data)
